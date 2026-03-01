7277f651bbd994a95f4fe2d73aa66d6d8eda4208
FoxFore32x <dgarciaromero10@gmail.com>
2026-03-01 00:28:13 -0800

    removed the build files lmao

--- GMA_Compiler/datafiles/Runner/build/cJSON.d
+++
@@ -1,4 +0,0 @@
-cJSON.o: \
- C:/Users/dgarc/OneDrive/Documentos/GitHub/GameMaker-Anywhere/GMA_Compiler/datafiles/Runner/source/cJSON.c \
- C:/Users/dgarc/OneDrive/Documentos/GitHub/GameMaker-Anywhere/GMA_Compiler/datafiles/Runner/source/cJSON.h
-C:/Users/dgarc/OneDrive/Documentos/GitHub/GameMaker-Anywhere/GMA_Compiler/datafiles/Runner/source/cJSON.h:
--- GMA_Compiler/datafiles/Runner/build/shortcut_functions.d
+++
@@ -1,4 +0,0 @@
-shortcut_functions.o: \
- C:/Users/dgarc/OneDrive/Documentos/GitHub/GameMaker-Anywhere/GMA_Compiler/datafiles/Runner/source/shortcut_functions.c \
- C:/Users/dgarc/OneDrive/Documentos/GitHub/GameMaker-Anywhere/GMA_Compiler/datafiles/Runner/source/shortcut_functions.h
-C:/Users/dgarc/OneDrive/Documentos/GitHub/GameMaker-Anywhere/GMA_Compiler/datafiles/Runner/source/shortcut_functions.h:
